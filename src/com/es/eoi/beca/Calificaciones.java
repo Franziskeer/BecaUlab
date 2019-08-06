7e2ef293f56d6f7825858fec2c4d5711abf46431
Franziskeer <fpa19@alu.ua.es>
2019-08-06 19:19:17 +0200

    Minimal improvement

--- src/com/es/eoi/beca/Calificaciones.java
+++ src/com/es/eoi/beca/Calificaciones.java
@@ -8,17 +8,17 @@ import java.util.Scanner;
 public class Calificaciones {
 
 	private static List<Alumno> calif = new ArrayList<>();
-	
+
 	private static final String SEPARATOR = "===============================================";
-	
-	public static void main(String[] args) {
-		printMenu();
-	}
 
 	@SuppressWarnings("resource")
-	public static void printMenu() {
-
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		printMenu(in);
+	}
+
+	public static void printMenu(Scanner in) {
+
 		System.out.println(SEPARATOR);
 		System.out.println("1 - Nueva calificación");
 		System.out.println("2 - Listar calificaciones");
@@ -30,15 +30,16 @@ public class Calificaciones {
 		System.out.println("8 - Listar INSUFICIENTES");
 		System.out.print("--> Introduce una opción: ");
 		Integer option = in.nextInt();
+		in.nextLine();
 
 		if (option < 1 || option > 7) {
 			System.out.println("ERROR: El valor de la opción no es correcto");
-			printMenu();
+			printMenu(in);
 		} else {
-			
+
 			switch (option) {
 			case 1:
-				newMark();
+				newMark(in);
 				break;
 
 			case 2:
@@ -48,58 +49,56 @@ public class Calificaciones {
 			case 3:
 				listAsc();
 				break;
-				
+
 			case 4:
 				listDes();
 				break;
-				
+
 			case 5:
 				listCriteria(Notas.INSUFICIENTE);
 				break;
-				
+
 			case 6:
 				listCriteria(Notas.SUFICIENTE);
 				break;
-				
+
 			case 7:
 				listCriteria(Notas.NOTABLE);
 				break;
-				
+
 			case 8:
 				listCriteria(Notas.SOBRESALIENTE);
 				break;
-				
+
 			default:
 				break;
 			}
-			
+
 		}
-		
-		printMenu();
-		
+
+		printMenu(in);
+
 	}
 
-	@SuppressWarnings("resource")
-	public static void newMark() {
-		
-		Scanner in = new Scanner(System.in);
-		System.out.println(SEPARATOR);		
+	public static void newMark(Scanner in) {
+
+		System.out.println(SEPARATOR);
 		System.out.println("HAS ELEGIDO INTRODUCIR NUEVA CALIFICACIÓN");
-		
+
 		System.out.print("Introduce el nombre del alumno: ");
 		String name = in.nextLine();
-		
+
 		System.out.print("Introduce una nota del 0 al 10: ");
 		Double mark = in.nextDouble();
 		in.nextLine();
 
 		Notas nota = null;
-		
+
 		if (mark < 0 || mark > 10) {
 			System.out.println("ERROR: El valor debe estar entre 0 y 10");
-			newMark();
+			newMark(in);
 		} else {
-			
+
 			if (mark >= 0 && mark < 5) {
 				nota = Notas.INSUFICIENTE;
 			} else if (mark >= 5 && mark < 7) {
@@ -111,21 +110,22 @@ public class Calificaciones {
 			}
 
 			calif.add(new Alumno(name, nota, mark));
+			
 		}
-		
+
 	}
-	
+
 	public static void listMarks() {
-		
+
 		System.out.println(SEPARATOR);
 		System.out.println("HAS ELEGIDO LISTAR CALIFICACIONES");
 
 		for (Alumno alumno : calif) {
 			System.out.println(alumno);
 		}
-		
+
 	}
-	
+
 	public static void listAsc() {
 
 		System.out.println(SEPARATOR);
@@ -133,38 +133,38 @@ public class Calificaciones {
 
 		List<Alumno> auxNotas = calif;
 		Collections.sort(auxNotas);
-		
+
 		for (Alumno alumno : auxNotas) {
 			System.out.println(alumno);
 		}
-		
+
 	}
-	
+
 	public static void listDes() {
 
 		System.out.println(SEPARATOR);
 		System.out.println("HAS ELEGIDO LISTAR CALIFICACIONES DESCENDENTEMENTE");
-		
+
 		List<Alumno> auxNotas = calif;
 		Collections.reverse(auxNotas);
-		
+
 		for (Alumno alumno : auxNotas) {
 			System.out.println(alumno);
 		}
 	}
 
 	public static void listCriteria(Notas nota) {
-		
+
 		System.out.println(SEPARATOR);
 		System.out.println("HAS ELEGIDO LISTAR CALIFICACIONES " + nota);
 
 		for (Alumno alumno : calif) {
 			if (alumno.getCalificacion().equals(nota)) {
-				System.out.println(alumno);	
+				System.out.println(alumno);
 			}
-			
+
 		}
-		
+
 	}
-	
+
 }
