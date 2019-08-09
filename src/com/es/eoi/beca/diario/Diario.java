619959c6a64025502ea56a0891da84f530d2b0af
franj <franj@DESKTOP-OHNLPTK>
2019-08-09 07:32:26 +0200

    Ejercicio del diario terminado

--- src/com/es/eoi/beca/diario/Diario.java
+++ src/com/es/eoi/beca/diario/Diario.java
@@ -8,43 +8,60 @@ public class Diario {
 	public static final String SEPARATOR = "===================================";
 
 	public static Scanner in = new Scanner(System.in);
-	
-	public static void main(String[] args) throws IOException  {
+
+	public static void main(String[] args) throws IOException {
 
 		printMenu();
-		in.close();
-		
+
 	}
 
 	private static void printMenu() throws IOException {
-		
+
 		System.out.println(SEPARATOR);
 		System.out.println("1 - Escribir diario");
 		System.out.println("2 - Leer diario");
+		System.out.println("3 - Borrar diario");
 		System.out.print("--> Introduce una opción: ");
-		Integer option = in.nextInt();
-		in.nextLine();
+
+		String option = in.nextLine();
+		Integer optionSwitch = 0;
 		
-		if (option == 1 || option == 2) {
-			switch (option) {
+		try {
+			
+			optionSwitch = Integer.parseInt(option);
+			
+		} catch (Exception e) {
+			
+			System.out.println(e);
+			
+		}
+
+		if (optionSwitch == 1 || optionSwitch == 2 || optionSwitch == 3) {
+			switch (optionSwitch) {
 			case 1:
 				writeDiary();
 				break;
 			case 2:
-				//readDiary();
+				readDiary();
 				break;
-
+			case 3:
+				eraseDiary();
+				break;
 			default:
 				break;
 			}
+			
+			printMenu();
+		} else {
+			System.out.println("ERROR: Opción inválida");
 		}
-		
-		printMenu();
 
 	}
 
+	
+
 	private static void writeDiary() throws IOException {
-		
+
 		FileWriter fw = null;
 		String line = "";
 
@@ -52,21 +69,80 @@ public class Diario {
 
 			fw = new FileWriter("./src/com/es/eoi/beca/diario/diary.txt", true);
 			System.out.println("Escribe a partir de aqui:");
-			
+
 			while (!line.equals("FIN")) {
 				line = in.nextLine();
-				fw.write(line+"\n");
+				fw.write(line + "\n");
 			}
-			
+
 			System.out.println("Se ha guardado correctamente");
 
 		} catch (Exception e) {
 			System.out.println(e);
 		} finally {
+
 			if (fw != null) {
 				fw.close();
 			}
+
+		}
+
+	}
+
+	private static void readDiary() throws IOException {
+
+		BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+
+            fr = new FileReader("./src/com/es/eoi/beca/diario/diary.txt");
+            br = new BufferedReader(fr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        } finally {
+        	
+            try {
+            	
+                if (br != null)
+                    br.close();
+
+                if (fr != null)
+                    fr.close();
+                
+            } catch (IOException ex) {
+                System.err.format("IOException: %s%n", ex);
+            }
+            
+        }
+        
+	}
+	
+	private static void eraseDiary() throws IOException {
+		
+		FileWriter fw = null;
+		
+		try {
+
+			fw = new FileWriter("./src/com/es/eoi/beca/diario/diary.txt", false);
+			System.out.println("Se ha borrado correctamente");
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+
+			if (fw != null) {
+				fw.close();
+			}
+
 		}
 		
 	}
+
 }
